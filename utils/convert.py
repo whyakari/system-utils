8f8f2cefdae893b63e710443e8b85b1e41a17984
akari <akariondev@gmail.com>
2023-11-06 02:57:11 +0000

    utils: convert: added new functions mb_to_bytes() and tb_to_bytes() for other types of uses.
    Signed-off-by: akari <akariondev@gmail.com>

--- utils/convert.py
+++ utils/convert.py
@@ -1,7 +1,25 @@
-from .types import gibibytes
+from .types import (
+    gibibytes, 
+    terabytes, 
+    megabytes
+)
+
+def tb_to_bytes(tb: terabytes) -> int:
+    """Converte terabytes em bytes."""
+    bytes_per_terabyte = 1099511627776
+    bytes = tb * bytes_per_terabyte
+    return bytes
+
 
 def gb_to_bytes(gb: gibibytes) -> int:
-    """function for converter gibibytes in bytes."""
+    """Converte gibibytes em bytes."""
     bytes_per_gibibyte = 1073741824
     bytes = gb * bytes_per_gibibyte
     return bytes
+
+def mb_to_bytes(mb: megabytes) -> int:
+    """Converte megabytes em bytes."""
+    bytes_per_megabyte = 1048576
+    bytes = mb * bytes_per_megabyte
+    return bytes
+
