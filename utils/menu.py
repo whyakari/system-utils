e5a3f77dafc54a7e5e3731375c32f457794230b5
akari <akariondev@gmail.com>
2024-02-02 22:05:51 +0000

    utils update
    Signed-off-by: akari <akariondev@gmail.com>

--- utils/menu.py
+++ utils/menu.py
@@ -1,23 +1,27 @@
 from .types import user
 from .clean import clean
 from .prompt import input_user
+from .colors import Colors
 
 def menu() -> user:
     """menu() is the menu main of the users."""
     clean()
-    menu_ = """
+
+    colors = Colors()
+
+    menu_ = f"""{colors.CYAN}
 ----------------------------------------
 Welcome to menu                        •
-of the system utils functions.         •
+System-Utils functions!                •
 © 2023 - あかり梓川.                   •
 ----------------------------------------
 
-[1] Convert MB's to bytes
-[2] Convert GB's to bytes
-[3] Convert TB's to bytes
-[4] Exit Program"""
+{colors.WHITE}[1] Convert MB's to bytes
+{colors.WHITE}[2] Convert GB's to bytes
+{colors.WHITE}[3] Convert TB's to bytes
+{colors.WHITE}[4] Other Functions {colors.RED}[beta]
+{colors.WHITE}[5] Exit"""
     print(menu_)
-
     option = input_user("\n⟩ ")
 
     if option == "":
--- utils/options.py
+++ utils/options.py
@@ -1,16 +1,23 @@
+from utils.colors import Colors
 from .menu import menu
 from .types import user
 from .prompt import input_user
-from .convert import gb_to_bytes, mb_to_bytes, tb_to_bytes
+
+from .convert import (
+    gb_to_bytes, 
+    mb_to_bytes, 
+    tb_to_bytes
+)
 
 def options(op: user) -> user:
     """options() is the choice options of the menu()."""
+
+    colors = Colors()
 
     match op:
 
         case "1":
             print("""Function: Megabytes to bytes""")
-
             option = input_user("Enter megabytes ⟩ ")
 
             if option == "":
@@ -18,12 +25,10 @@ def options(op: user) -> user:
 
             result = mb_to_bytes(int(option))
             input(f"{option}MB (megabytes) is equal the {result} bytes.")
-
             return options(menu())
 
         case "2":
             print("""Function: Gibibytes to bytes.""")
-            
             option = input_user("Enter gibibytes ⟩ ")
 
             if option == "":
@@ -31,13 +36,10 @@ def options(op: user) -> user:
 
             result = gb_to_bytes(int(option))
             input(f"{option}GB (gibibytes) is equal the {result} bytes.")
-
-            # back to menu.
             return options(menu())
 
         case "3":
             print("""Function: Terabytes to bytes""")
-
             option = input_user("Enter terabytes ⟩ ")
 
             if option == "":
@@ -45,10 +47,13 @@ def options(op: user) -> user:
 
             result = tb_to_bytes(int(option))
             input(f"{option}TB (terabytes) is equal the {result} bytes.")
-
             return options(menu())
 
         case "4":
+            input(f"{colors.YELLOW}This place is still unavailable, wait for new releases (:.")
+            return options(menu())
+
+        case "5":
             print("Bye... •́⁠ ⁠ ⁠‿⁠ ⁠,⁠•̀")
 
     return op
